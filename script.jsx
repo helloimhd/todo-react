5a488689d7e213f54695c8d22f47d76851d4d0bc
helloimhd <herda58@gmail.com>
2019-05-17 09:13:54 +0800

    clean up

--- script.jsx
+++ script.jsx
@@ -1,110 +1,3 @@
-
-class List extends React.Component {
-        constructor(){
-    super()
-    this.state = {
-        list: [],
-        word: ""
-    }
-
-
-    this.changeHandler = this.changeHandler.bind( this );
-
-    this.takeItem = this.takeItem.bind( this );
-
-    this.takeItemOnEnter = this.takeItemOnEnter.bind( this );
-
-    this.remove = this.remove.bind( this );
-  }
-
-
-
-  changeHandler(event){
-    this.setState({word:event.target.value});
-    console.log("change", event.target.value);
-  }
-
-  takeItemOnEnter(e) {
-    let word = e.target.value
-    //console.log(word.length)
-
-    if (e.key === 'Enter') {
-        if (word.length == 1 || word.length > 10) {
-            alert("Word must be more that 1 and less than 10 characters")
-        } else {
-            let listObj = {}
-           // console.log(this.state.word)
-            const list = this.state.list
-            let word = this.state.word
-
-            listObj["word"] = word;
-            listObj["moment"] = moment().format('MMMM Do YYYY, h:mm:ss a');
-
-            list.push(listObj)
-
-            // clear the input
-            this.setState({word: ""})
-        }
-    }
-  }
-
-  takeItem() {
-    let listObj = {}
-    const list = this.state.list
-    let word = this.state.word
-
-    listObj["word"] = word;
-    listObj["moment"] = moment().format('MMMM Do YYYY, h:mm:ss a');
-
-    list.push(listObj)
-
-    // clear the input
-    this.setState({word: ""})
-  }
-
-  remove(e) {
-    var list = this.state.list
-    //document.getElementById("itemTable").deleteRow(parseInt(e.target.id) +   1)
-    //alert("click", e.id)
-    list.splice(e.target.id, 1)
-    console.log(list)
-    this.setState({list: list})
-
-  }
-
-  render() {
-      // render the list with a map() here
-      let allItems = this.state.list.map( (obj, index) => {
-            return <tr>
-            <td>{obj.word}</td>
-            <td><button id={index} onClick={this.remove}>❌</button></td>
-            <td>{obj.moment}</td>
-            </tr>
-        }) // end of map
-
-      //console.log("rendering");
-      return (
-        <div className="list">
-
-
-
-          <table id="itemTable">
-            <thead>
-                <tr>
-                    <th>Items</th>
-                    <th>Delete</th>
-                    <th>Moment</th>
-                </tr>
-            </thead>
-            <tbody>
-                {allItems}
-            </tbody>
-          </table>
-        </div>
-      );
-  }
-}
-
 /////////////////////////////////////////////
 class Form extends React.Component {
     render() {
@@ -136,15 +29,28 @@ class ItemList extends React.Component {
     render() {
         {this.storeInList()}
         console.log(this.state.list)
-        let allItems = this.state.list.map(list => {
+        let allItems = this.state.list.map((list, index) => {
             return (
-                <li>{list}</li>
+                <tr>
+                    <td>{list}</td>
+                    <td>{index}</td>
+                </tr>
             )
         }) // end of map
         return (
-            <ul>
-                {allItems}
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Items</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {allItems}
+                </tbody>
+            </table>
+
         )
     }
 }
